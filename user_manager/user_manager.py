28690b3e47ac3b631a2c2407c4beb8770e062e3c
m-tehrani <t.mohammadamin@gmail.com>
2021-11-04 14:13:58 +0330

    refactor:  User class add email, password, username

--- user_manager/user_manager.py
+++ user_manager/user_manager.py
@@ -2,10 +2,13 @@ import pickle
 
 
 class User:
-    def __init__(self, id, name, phone):
+    def __init__(self, id, username, name, phone, password, email=None):
         self.id = id
+        self.username = username
         self.name = name
         self.phone = phone
+        self.password = password
+        self.email = email
 
     def save(self):
         file_name = f'{self.id}.user'
