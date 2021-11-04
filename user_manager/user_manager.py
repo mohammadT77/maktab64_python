7bd06a8222b026f4baca4434099a685432e879fb
m-tehrani <t.mohammadamin@gmail.com>
2021-11-04 14:18:28 +0330

    feat: UserError.str

--- user_manager/user_manager.py
+++ user_manager/user_manager.py
@@ -26,3 +26,9 @@ class UserError(Exception):
 
     def __init__(self, msg: str, field: str, data: ...) -> None:
         super().__init__(msg, field, data)
+        self.msg = msg
+        self.field = field
+        self.data = data
+
+    def __str__(self):
+        return f"User data error on field `{self.field}` (invalid data: `{self.data}`):\n{self.msg}"
