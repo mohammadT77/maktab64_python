608ee5e19d3c193993340251dab341c751fdc9b1
m-tehrani <t.mohammadamin@gmail.com>
2021-11-18 15:03:18 +0330

    fix: assert step != 0

--- range/range_gen.py
+++ range/range_gen.py
@@ -2,6 +2,7 @@ def range(start, end: int = None, step=1):
     if end is None:
         start, end = 0, start
 
+    assert step != 0, "Step cannot be 0 (inf loop)"
     i = start
     while i < end:
         yield i
