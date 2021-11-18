e26232940ab88c7deb0ae2143071a0d3e7156009
m-tehrani <t.mohammadamin@gmail.com>
2021-11-18 16:04:36 +0330

    feat: main

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ class MyIterClass:  # Iterable!
     #     return self.n
 
 
-m_iter = iter(MyIterClass())
-print(m_iter)
-print(next(m_iter)) # a
-print(next(m_iter)) # s
-print(next(m_iter))
+# m_iter = iter(MyIterClass())
+# print(m_iter)
+# print(next(m_iter)) # a
+# print(next(m_iter)) # s
+# print(next(m_iter))
 
+def avg(*args):
+    return sum(args) / len(args)
+
+def register_user(*args, **kwargs):
+    print(kwargs)
+
+register_user(1,2,3,4,5,5,6,7,7, name='sdsadsa', email='asdsada', age=123)
